c64967d2f78dfc9c3ff3d446a3f96059464dc611
gorann1 <goran.novacic@gmail.com>
2025-04-24 17:20:22 +0200

    first_init_commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from "path"
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import RubyPlugin from 'vite-plugin-ruby'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    RubyPlugin(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app/frontend/"),
+    },
+  },
+})
